4354b7f3c2342591273680c5eb31502b215e2a93
Konstantinos <michos.constantinos@gmail.com>
2022-05-17 16:21:48 +0300

    Moved Tests class to proper package.

--- src/test/java/com/demo/myproject/utils/DemoApplicationTests.java
+++ src/test/java/com/demo/myproject/utils/DemoApplicationTests.java
@@ -1,9 +1,11 @@
-package com.demo.myproject;
+package com.demo.myproject.utils;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoApplicationTests {
 
 	@Test
